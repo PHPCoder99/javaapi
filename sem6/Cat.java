d7b09d887f24d34c64725513e537843d6722c2ee
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-07-14 13:23:55 +0700

    6

---
+++ sem6/Cat.java
@@ -0,0 +1,55 @@
+package sem6;
+
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+    private Integer age;
+    private String color;
+
+    public Cat(String name, Integer age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name) && Objects.equals(age, cat.age) && Objects.equals(color, cat.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, color);
+    }
+}
